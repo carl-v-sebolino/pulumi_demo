0e69aac610f92647d94b87f4e53a3a06e4c497cc
Carl Sebolino <carl.v.sebolino@accenture.com>
2023-07-12 14:49:26 +0800

    add config sample code

--- __main__.py
+++ __main__.py
@@ -1,15 +1,13 @@
 """An AWS Python Pulumi program"""
 
 import pulumi
-from pulumi_aws import s3
 
-# Create an AWS resource (S3 Bucket)
-# create an instance of the aws s3 bucket
-bucket = s3.Bucket('my-bucket')
-"""
-The same as
-s3.Bucket('my-bucket')
-"""
+config = pulumi.Config()
+required_value = config.require('required_value')
+optional_value = config.get('optional_value')
+secret_value = config.require_secret('secret_value')
 
-# Export the name of the bucket
-pulumi.export('bucket_name', bucket.id)
+
+print(required_value)
+print(optional_value)
+print(secret_value)
