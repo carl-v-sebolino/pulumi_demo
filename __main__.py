2330c1432f75ae03183d8ee04391d3bf75712621
Carl Sebolino <carl.v.sebolino@accenture.com>
2023-07-12 15:20:32 +0800

    clear main.py

--- __main__.py
+++ __main__.py
@@ -1,13 +1,1 @@
-"""An AWS Python Pulumi program"""
-
 import pulumi
-
-config = pulumi.Config()
-required_value = config.require('required_value')
-optional_value = config.get('optional_value')
-secret_value = config.require_secret('secret_value')
-
-
-print(required_value)
-print(optional_value)
-print(secret_value)
